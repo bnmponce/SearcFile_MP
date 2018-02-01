c406bddcbd4b15997ab15c64a20724ccd550e826
GladysCopaga <glacch7@gmail.com>
2018-01-31 22:32:10 -0400

    added the arguments for: size, extension added inputs for: size, extension added logic to search by filters (size, extension) in __main__.py

--- search_files_mag/src/com/jalasoft/search_files/menu/menu_options.py
+++ search_files_mag/src/com/jalasoft/search_files/menu/menu_options.py
@@ -12,27 +12,50 @@ class PrintMenu(object):
         )
         parser.add_argument(
             '-s',
-            dest = 'search',
-            action = 'store_true',
-            help = 'perform search with criteria given.',
+            dest='search',
+            action='store_true',
+            help='perform search with criteria given.',
         )
         parser.add_argument(
             '--name',
             '-n',
-            default = menu_options.get_name(),
-            help = 'Configure the name of the file or folder to search.',
+            default=menu_options.get_name(),
+            help='Configure the name of the file or folder to search.',
         )
         parser.add_argument(
             '--path',
             '-p',
-            default = menu_options.get_path(),
+            default=menu_options.get_path(),
             help='Configure the path in order to search a file or folder.',
         )
         parser.add_argument(
             '--type',
             '-t',
-            default = menu_options.get_type_search(),
+            default=menu_options.get_type_search(),
             help='Configure the type of search file or folder to search.',
         )
+        parser.add_argument(
+            '--size',
+            '-z',
+            default=menu_options.get_size(),
+            help='Configure the file size (Mb) to search. If you specify only the size, it will search by default'
+                 ' the files size less than the size specified. Otherwise use the following operators: -l, -g or -e'
+        )
+        parser.add_argument(
+            '--operator',
+            '-o',
+            default='l',
+            help='enter the operator to search by specific size:'
+                 ' -l: to search files less than the size specified'
+                 ' -g: to search the files greater than the size specified'
+                 ' -e: to search the exact match size ',
+        )
+        parser.add_argument(
+            '--extension',
+            '-ex',
+            # default=menu_options.get_extension(),
+            help='Configure the file extension to search. It will search all file types by default if none is given',
+        )
+
         args = parser.parse_args()
         return args
