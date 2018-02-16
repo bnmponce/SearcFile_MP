2aa98e6f1ffba43be33ee8d8649e397caa659cd6
ArielZurita <ariel.zmerida@gmail.com>
2018-02-15 20:24:58 -0400

    Creating search_util.py file and SearchUtil class in order to have the util methods in another class also update some unittest

---
+++ search_files_mag/src/com/jalasoft/search_files/utils/search_utils.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from src.com.jalasoft.search_files.utils.logging import logger
+
+class SearchUtil():
+
+    def convert_date(self, date_in_float):
+        """
+        This method convert a date in float a string "%d-%b-%Y"; %d = day, %b = month and %Y = year
+        :param date_in_float: date in float
+        :return: return a date in string
+        """
+        logger.info("convert_date: Enter with date to convert %s" % date_in_float)
+        time_in_date = datetime.fromtimestamp(date_in_float)
+        date = datetime.strftime(time_in_date, '%m%d%Y')
+        logger.info("convert_date: Exit > Returning date %s" % date)
+        return date
